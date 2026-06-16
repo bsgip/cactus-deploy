16d749768cf982293a8cdd9f329378f725bdb6ba
Josh Vote <jjvote@gmail.com>
2026-06-16 16:29:32 +1000

    Tagging release-158

--- docker/cactus-teststack-init/entrypoint.sh
+++ docker/cactus-teststack-init/entrypoint.sh
@@ -12,7 +12,16 @@ until psql ${ENVOY_DATABASE_URL} -c "SELECT 1;" >/dev/null 2>&1; do
   sleep 1
 done
 
+set -e
+
 echo "Running migrations..."
 psql ${ENVOY_DATABASE_URL} -f /migrate.sql
+
+if [ -n "$MIGRATION_SENTINEL" ]; then
+  echo "Recording completion at $MIGRATION_SENTINEL"
+  mkdir -p "$(dirname "$MIGRATION_SENTINEL")" && touch "$MIGRATION_SENTINEL"
+else
+  echo "MIGRATION_SENTINEL is NOT set - not recording completion"
+fi
 
 echo "End of teststack-init"
--- docker/envoy/entrypoint.sh
+++ docker/envoy/entrypoint.sh
@@ -1,5 +1,24 @@
 #!/bin/bash
 
+# Wait for the DB migration sentinel to be created
+if [ -n "$MIGRATION_SENTINEL" ]; then
+    MAX_MIGRATION_ATTEMPTS=1200
+    MIGRATION_ATTEMPT=0
+    while [ ! -f "$MIGRATION_SENTINEL" ]; do
+        echo "Waiting for database migrations via $MIGRATION_SENTINEL"
+        sleep 0.1
+        MIGRATION_ATTEMPT=$((MIGRATION_ATTEMPT + 1))
+        if [ $MIGRATION_ATTEMPT -ge $MAX_MIGRATION_ATTEMPTS ]; then
+            echo "Timed out waiting for database migrations!" >&2
+            exit 1
+        fi
+    done
+
+    echo "$MIGRATION_SENTINEL exists - database should be migrated."
+else
+    echo "MIGRATION_SENTINEL has not been specified. Skipping wait on migration."
+fi
+
 if [ -n "${RABBIT_MQ_BROKER_URL}" ]; then
     echo "Notifications are enabled. RABBIT_MQ_BROKER_URL has been set."
 
